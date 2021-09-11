daec72a6299bf2923fdab36c5d388a4bc0520d3b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-11 19:09:20 +0530

    [Shantanu] Added :  Properties for user registration with data annotations into bussiness layer.

--- CommonLayer/UserModel.cs
+++ CommonLayer/UserModel.cs
@@ -7,26 +7,26 @@ namespace CommonLayer
 {
     public class UserModel
     {
-        [Required]
+        [Required(ErrorMessage = "First Name is required.")]
         [RegularExpression("^[A-Z][a-z]{1,}",ErrorMessage ="Name should start with capital letters and" +
             "should contain atleast 2 characters.")]
         [DataType(DataType.Text)]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Last Name is required.")]
         [RegularExpression("^[A-Z][a-z]{1,}", ErrorMessage = "Name should start with capital letters and" +
             "should contain atleast 2 characters.")]
+        [DataType(DataType.Text)]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Email Address is required.")]
         [EmailAddress]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required]
-        [RegularExpression("^((?=.*[A-Z][a-z])(?=.*[0-9])(?=.*[!@#$%^&*-_.])(?=.{8,}))", 
-            ErrorMessage = "Password should contain minimum 8 character atleast one upper case character" +
-            " and atleast one numeric value")]
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression("^((?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])|(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[^a-zA-Z0-9])|(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9])).{8,}$",
+            ErrorMessage = "Passwords must be at least 8 characters and contain at 3 of 4 of the following: upper case (A-Z), lower case (a-z), number (0-9) and special character (e.g. !@#$%^&*)")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
