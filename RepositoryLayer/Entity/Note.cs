06e700d1cbb68bfb0bc97d2866d080de59c36537
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-15 19:04:22 +0530

    [Shantanu] Added : Notes entity for adding specified colunms in notes table.

---
+++ RepositoryLayer/Entity/Note.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace RepositoryLayer.Entity
+{
+    public class Note
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Remainder { get; set; }
+
+        public string Color { get; set; }
+
+        public string image { get; set; }
+
+        public bool isArchive { get; set; }
+
+        public bool isTrash { get; set; }
+
+        public bool isPin { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? ModifiedAt { get; set; }
+
+        [ForeignKey("User")]
+        public long UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
