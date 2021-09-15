18409dbdea993811c1bf1ce660a83f5a40775a45
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-15 18:57:05 +0530

    [Shantanu] Added :  Interface for CRUD opertion for notes.

---
+++ BusinessLayer/Interface/INoteBL.cs
@@ -0,0 +1,19 @@
+﻿using CommonLayer;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface INoteBL
+    {
+        List<Note> GetAllNotes();
+
+        bool AddNotes(NotesModel notesModel, long Id);
+
+        public bool DeleteNotes(long id, long userId);
+
+        public bool UpdateNotes(long id, long userId, NotesModel notesModel);
+    }
+}
