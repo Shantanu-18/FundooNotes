95aa574e459ebe8a06eb8313c67019f62efa3c46
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-15 18:58:44 +0530

    [Shantanu] Added : Notes model in common layer.

---
+++ CommonLayer/NotesModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class NotesModel
+    {
+        public string Title { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime Remainder { get; set; }
+
+        public string Color { get; set; }
+
+        public string image { get; set; }
+
+        public bool isArchive { get; set; }
+
+        public bool isTrash { get; set; }
+
+        public bool isPin { get; set; }
+    }
+}
