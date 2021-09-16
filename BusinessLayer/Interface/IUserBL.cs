2058d24bd430de1ae03d9fb9addc36678b7c5334
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-16 21:26:00 +0530

    [Shantanu] Added: Forgot and reset password interface in BL.

--- BusinessLayer/Interface/IUserBL.cs
+++ BusinessLayer/Interface/IUserBL.cs
@@ -9,7 +9,13 @@ namespace BusinessLayer.Interface
     public interface IUserBL
     {
         List<UserModel> getAllUsers();
+
         bool RegisterUser(UserModel userModel);
+
         User UserLogIn(LogInModel logInModel);
+
+        User ForgotPassword(ForgotPassModel forgotPassModel);
+
+        User ResetPassword(ResetPasswordModel resetPasswordModel, long UserId);
     }
 }
