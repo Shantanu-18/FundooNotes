0d1ffe2f89f6c548a483cad2dcd5afa07b048593
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-16 21:29:00 +0530

    [Shantanu] Added: Forgot password model.

---
+++ CommonLayer/ForgotPassModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class ForgotPassModel
+    {
+        [Required]
+        public string Email { get; set; }
+    }
+}
