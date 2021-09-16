58f9cef37eab96f1e34ae9488dd83b00b750a4c9
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-16 21:30:52 +0530

    [Shantanu] Added: MSMQ operations for sending mail.

---
+++ CommonLayer/MSMQ/MsmqOperations.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Experimental.System.Messaging;
+
+namespace CommonLayer.MSMQ
+{
+    public class MsmqOperations
+    {
+        MessageQueue msmq = new MessageQueue();
+        public void SendingData(string token)
+        {
+            msmq.Path = @".\private$\tohenQueue";
+
+            if (!MessageQueue.Exists(msmq.Path))
+            {
+                //if not exixts
+                MessageQueue.Create(msmq.Path);
+            }
+            SendingToken(token);
+        }
+
+        public void SendingToken(string token)
+        {
+            //For adding Xml formatter to the message
+            msmq.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+
+            msmq.ReceiveCompleted += Msmq_ReceiveCompleted;
+
+            //for sending token to the queue
+            msmq.Send(token);
+            msmq.BeginReceive();
+            msmq.Close();
+        }
+
+        private void Msmq_ReceiveCompleted(object sender, ReceiveCompletedEventArgs e)
+        {
+            try
+            {
+                //getting  token from receiver
+                var msg = msmq.EndReceive(e.AsyncResult);
+
+                string token = msg.Body.ToString();
+                //sending a mail via SMTP
+
+                mailSending(token);
+                msmq.BeginReceive();
+            }
+            catch (MessageQueueException ex)
+            {
+                throw ex;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void mailSending(string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
