a4c32049881b5b9fa800814fae002167d7250d61
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-16 21:28:08 +0530

    [Shantanu] Added: Forgot and reset password interface.

--- RepositoryLayer/Interface/IUserRL.cs
+++ RepositoryLayer/Interface/IUserRL.cs
@@ -9,7 +9,13 @@ namespace BusinessLayer.Interface
     public interface IUserRL
     {
         List<UserModel> getAllUsers();
+
         bool RegisterUser(UserModel userModel);
+
         User UserLogIn(LogInModel logInModel);
+
+        User ForgotPassword(ForgotPassModel forgotPassModel);
+
+        User ResetPassword(ResetPasswordModel resetPasswordModel, long UserId);
     }
 }
