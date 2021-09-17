7cd73a917f3a4e4b8fd39c0a9f911df5ab02581f
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-17 20:03:01 +0530

    [Shantanu] Added: Sending mail method using smtp

--- CommonLayer/MSMQ/MsmqOperations.cs
+++ CommonLayer/MSMQ/MsmqOperations.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Text;
 using Experimental.System.Messaging;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using Microsoft.Extensions.Options;
 
 namespace CommonLayer.MSMQ
 {
     public class MsmqOperations
     {
         MessageQueue msmq = new MessageQueue();
+        private readonly Smtp _smtp;
+        public MsmqOperations(IOptions<Smtp> options)
+        {
+            _smtp = options.Value;
+        }
+
+        public MsmqOperations()
+        {
+        }
+
         public void SendingData(string token)
         {
-            msmq.Path = @".\private$\tohenQueue";
+            msmq.Path = @".\private$\tokenQueue";
 
             if (!MessageQueue.Exists(msmq.Path))
             {
@@ -58,7 +72,32 @@ namespace CommonLayer.MSMQ
 
         private void mailSending(string token)
         {
-            throw new NotImplementedException();
+            MailMessage mailMessage = new MailMessage("sendertohost@gmail.com", "sendertohost@gmail.com");
+            mailMessage.Subject = "Reset password link.";
+
+            var body = new StringBuilder();
+            
+            body.AppendLine("Hello, To Reset your Account Password click the link below.");
+            body.AppendLine("<a href=\"https://localhost:5001/User/ResetPassword/"+token+"\">Click Here</a>");
+            mailMessage.Body = body.ToString();
+            mailMessage.IsBodyHtml = true;
+
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+
+            smtpClient.Credentials = new NetworkCredential()
+            {
+                UserName = "sendertohost@gmail.com",
+                Password = "Test@123"
+            };
+
+            //var smtpClient = new SmtpClient(_smtp.Host)
+            //{
+            //    Port = _smtp.Port,
+            //    Credentials = new NetworkCredential(_smtp.UserName, _smtp.Password),
+            //    EnableSsl = true,
+            //};
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(mailMessage);
         }
     }
 }
