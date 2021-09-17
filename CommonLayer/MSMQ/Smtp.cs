2fdc106613831f293dd236d4b2cd30a2df6d2a73
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-17 20:03:41 +0530

    [Shantanu] Added: Smtp model

---
+++ CommonLayer/MSMQ/Smtp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer.MSMQ
+{
+    public class Smtp
+    {
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
