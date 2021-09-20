d3d95a9ec45a3e4189bbb03788c62e9948cb5cb9
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:16:15 +0530

    [Shantanu] Added : Implementation of add collab interface.

---
+++ BusinessLayer/Services/CollaborationBL.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Interface;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class CollaborationBL : ICollaborationBL
+    {
+        private ICollaborationRL _collaborationRL;
+        private INoteRL _noteRL;
+        private IUserRL _userRL;
+
+        public CollaborationBL(ICollaborationRL collaborationRL,INoteRL noteRL,IUserRL userRL)
+        {
+            _collaborationRL = collaborationRL;
+            _noteRL = noteRL;
+            _userRL = userRL;
+        }
+
+        public bool AddCollab(long noteId, long userId, string collabEmail)
+        {
+            try
+            {
+                var checkNote = _noteRL.GetNoteId(noteId, userId);
+                var checkEmail = _userRL.GetEmail(collabEmail);
+
+                if (checkNote == null)
+                {
+                    return false;
+                }
+
+                if (checkEmail == null)
+                {
+                    return false;
+                }
+                return _collaborationRL.AddCollab(noteId, userId, collabEmail);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ RepositoryLayer/Services/CollaborationRL.cs
@@ -0,0 +1,42 @@
+﻿using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class CollaborationRL : ICollaborationRL
+    {
+        private UserContext _userContext;
+        Collaboration collaboration = new Collaboration();
+
+        public CollaborationRL(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public bool AddCollab(long noteId, long userId, string collabEmail)
+        {
+            try
+            {
+                collaboration.CollabEmail = collabEmail;
+                collaboration.UserId = userId;
+                collaboration.NoteId = noteId;
+                _userContext.Collaborations.Add(collaboration);
+
+                var changes = _userContext.SaveChanges();
+
+                if (changes > 0) return true;
+
+                else return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
