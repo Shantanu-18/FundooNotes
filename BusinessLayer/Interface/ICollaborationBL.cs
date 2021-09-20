6d3984eeab4bb796a796c99df0077cd6095421f9
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:06:53 +0530

    [Shantanu] Added : Collaboration interface.

---
+++ BusinessLayer/Interface/ICollaborationBL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface ICollaborationBL
+    {
+        bool AddCollab(long noteId, long userId, string collabEmail);
+    }
+}
---
+++ RepositoryLayer/Interface/ICollaborationRL.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ICollaborationRL
+    {
+        bool AddCollab(long noteId, long userId, string collabEmail);
+    }
+}
