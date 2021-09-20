ea28536586e55c9a90641220cc635b72e6128e03
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:21:02 +0530

    [Shantanu] Added : migrations for collaboration table.

---
+++ RepositoryLayer/Migrations/20210919182056_AddedCollabationTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RepositoryLayer.Migrations
+{
+    public partial class AddedCollabationTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Collaborations",
+                columns: table => new
+                {
+                    CollabId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CollabEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoteId = table.Column<long>(type: "bigint", nullable: false),
+                    UserId = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Collaborations", x => x.CollabId);
+                    table.ForeignKey(
+                        name: "FK_Collaborations_Notes_NoteId",
+                        column: x => x.NoteId,
+                        principalTable: "Notes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Collaborations_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collaborations_NoteId",
+                table: "Collaborations",
+                column: "NoteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Collaborations_UserId",
+                table: "Collaborations",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Collaborations");
+        }
+    }
+}
