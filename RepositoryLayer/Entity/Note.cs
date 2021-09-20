4f14a940ef7465d5d5f68c897f911fa0d94f08dc
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:18:19 +0530

    [Shantanu] Added : property to take note id as fk in collaboration table.

--- RepositoryLayer/Entity/Note.cs
+++ RepositoryLayer/Entity/Note.cs
@@ -36,5 +36,7 @@ namespace RepositoryLayer.Entity
         public long UserId { get; set; }
 
         public virtual User User { get; set; }
+
+        public ICollection<Collaboration> Collaborations { get; set; }
     }
 }
