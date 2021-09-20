e347e03efb3618606ec76809c343267cadcd77b6
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 20:12:20 +0530

    [Shantanu] Added : Get collaboration interface.

--- BusinessLayer/Interface/ICollaborationBL.cs
+++ BusinessLayer/Interface/ICollaborationBL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RepositoryLayer.Entity;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,5 +8,7 @@ namespace BusinessLayer.Interface
     public interface ICollaborationBL
     {
         bool AddCollab(long noteId, long userId, string collabEmail);
+
+        List<Collaboration> GetCollab(long noteId, long userId);
     }
 }
