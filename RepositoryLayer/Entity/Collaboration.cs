96283d582aff6c3f1907f1b87c9c418a9c171268
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:08:43 +0530

    [Shantanu] Added :  Collaboration entity with required table column names.

---
+++ RepositoryLayer/Entity/Collaboration.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepositoryLayer.Entity
+{
+    public class Collaboration
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long CollabId { get; set; }
+
+        public string CollabEmail { get; set; }
+
+        public long NoteId { get; set; }
+
+        public Note Notes { get; set; }
+
+        public long? UserId { get; set; }
+
+        public User Users { get; set; }
+    }
+}
