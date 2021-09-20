b0031b143751e6796a3ea5915a7fce3bf0cd291d
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-20 19:16:57 +0530

    [Shantanu] Added : collaboration model with email property in it.

---
+++ CommonLayer/CollaborationModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommonLayer
+{
+    public class CollaborationModel
+    {
+        public string collabEmail { get; set; }
+    }
+}
