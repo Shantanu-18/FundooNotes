27348ade34210ff7c8f37f1f210007a8149b1054
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-23 10:46:15 +0530

    [Shantanu] Added : Add Label interface.

---
+++ BusinessLayer/Interface/ILabelBL.cs
@@ -0,0 +1,12 @@
+﻿using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface ILabelBL
+    {
+        bool AddLabel(long noteId, long userId, LabelModel labelModel);
+    }
+}
---
+++ RepositoryLayer/Interface/ILabelRL.cs
@@ -0,0 +1,12 @@
+﻿using CommonLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RepositoryLayer.Interface
+{
+    public interface ILabelRL
+    {
+        bool AddLabel(long noteId, long userId, LabelModel labelModel);
+    }
+}
