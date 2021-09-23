3fbef42ba31e291eeadc0f6d98b113d80f964406
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-23 10:46:52 +0530

    [Shantanu] Added : Implementation of Add Label interface.

---
+++ BusinessLayer/Services/LabelBL.cs
@@ -0,0 +1,30 @@
+﻿using BusinessLayer.Interface;
+using CommonLayer;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class LabelBL : ILabelBL
+    {
+        private ILabelRL _labelRL;
+
+        public LabelBL(ILabelRL labelRL)
+        {
+            _labelRL = labelRL;
+        }
+        public bool AddLabel(long noteId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                return _labelRL.AddLabel(noteId,userId,labelModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ RepositoryLayer/Services/LabelRL.cs
@@ -0,0 +1,52 @@
+﻿using CommonLayer;
+using RepositoryLayer.Context;
+using RepositoryLayer.Entity;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RepositoryLayer.Services
+{
+    public class LabelRL : ILabelRL
+    {
+        private UserContext _userContext;
+        Label label = new Label();
+
+        public LabelRL(UserContext userContext)
+        {
+            _userContext = userContext;
+        }
+
+        public bool AddLabel(long noteId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                var checkLabel = _userContext.Labels.FirstOrDefault(l => l.LabelName == labelModel.labelName && l.UserId == userId);
+
+                if (checkLabel == null)
+                {
+                    label.LabelName = labelModel.labelName;
+                    label.NoteId = noteId;
+                    label.UserId = userId;
+
+                    _userContext.Labels.Add(label);
+                    int changes = _userContext.SaveChanges();
+
+                    if (changes > 0) return true;
+
+                    else return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
