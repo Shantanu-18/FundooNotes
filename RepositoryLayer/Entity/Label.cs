7fc812320a789f0e54e7a16db54b3f69e27a201c
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-23 10:49:08 +0530

    [Shantanu] Added : Entity for Labels table with required column names as properties.

---
+++ RepositoryLayer/Entity/Label.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RepositoryLayer.Entity
+{
+    public class Label
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long LabelId { get; set; }
+
+        public string LabelName { get; set; }
+
+        public long NoteId { get; set; }
+
+        public Note Notes { get; set; }
+
+        public long? UserId { get; set; }
+
+        public User Users { get; set; }
+    }
+}
