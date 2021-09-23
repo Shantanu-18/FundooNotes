e4be82ec7ce3ea184760e6cffac3f39e0f0c480e
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-23 20:33:13 +0530

    [Shantanu] Added : GetNoteLables, AddLabelToUser, GetUserLabels API's.

--- FundooNotes/Controllers/LabelController.cs
+++ FundooNotes/Controllers/LabelController.cs
@@ -3,6 +3,7 @@ using CommonLayer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using RepositoryLayer.Entity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,19 +29,15 @@ namespace FundooNotes.Controllers
             return Convert.ToInt64(User.FindFirst("Id").Value);
         }
 
-        private string GetEmail()
-        {
-            return User.FindFirst(ClaimTypes.Email).Value.ToString();
-        }
-
         [HttpPost]
-        [Route("{noteId}")]
+        [Route("{noteId}/note")]
         public IActionResult AddLabel(long noteId, LabelModel labelModel)
         {
-            long userId = GetTokenId();
-            var result = _labelBL.AddLabel(noteId, userId, labelModel);
             try
             {
+                long userId = GetTokenId();
+                var result = _labelBL.AddLabel(noteId, userId, labelModel);
+
                 if (result == true)
                 {
                     return Ok(new { Success = true, message = "Label added Successfully !!" });
@@ -48,6 +45,85 @@ namespace FundooNotes.Controllers
                 else
                 {
                     return BadRequest(new { Success = false, message = "Label already exists !!" });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Success = false, message = e.Message, stackTrace = e.StackTrace });
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult AddLabelToUser(LabelModel labelModel)
+        {
+            try
+            {
+                long userId = GetTokenId();
+                bool result = _labelBL.AddLabelToUser(userId, labelModel);
+
+                if (result == true)
+                {
+                    return Ok(new { Success = true, message = "Label added Successfully !!" });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Label already exists !!" });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Success = false, message = e.Message, stackTrace = e.StackTrace });
+            }
+        }
+
+        [HttpGet]
+        [Route("{noteId}")]
+        public IActionResult GetNoteLables(long noteId)
+        {
+            try
+            {
+                long userId = GetTokenId();
+                List<Label> labelList = _labelBL.GetNoteLables(noteId, userId);
+
+                if (labelList.Count != 0)
+                {
+                    return this.Ok(new { Success = true, message = "These are Your all the Labels.", Data = labelList });
+                }
+                else if (labelList.Count == 0)
+                {
+                    return BadRequest(new { Success = false, message = "No Label is added to this note." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Something went wrong." });
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { Success = false, message = e.Message, stackTrace = e.StackTrace });
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetUserLabels()
+        {
+            try
+            {
+                long userId = GetTokenId();
+                List<Label> labelList = _labelBL.GetUserLabels(userId);
+
+                if (labelList.Count != 0)
+                {
+                    return this.Ok(new { Success = true, message = "These are Your all the Labels.", Data = labelList });
+                }
+                else if (labelList.Count == 0)
+                {
+                    return BadRequest(new { Success = false, message = "No Label is added." });
+                }
+                else
+                {
+                    return BadRequest(new { Success = false, message = "Something went wrong." });
                 }
             }
             catch (Exception e)
