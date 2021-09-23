4e9f00d9dfa7ef379dbdaf9f781f2fc4e9280438
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-23 20:32:59 +0530

    [Shantanu] Added : Implementation of GetNoteLables, AddLabelToUser, GetUserLabels interfaces.

--- BusinessLayer/Services/LabelBL.cs
+++ BusinessLayer/Services/LabelBL.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Interface;
 using CommonLayer;
+using RepositoryLayer.Entity;
 using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
@@ -20,6 +21,42 @@ namespace BusinessLayer.Services
             try
             {
                 return _labelRL.AddLabel(noteId,userId,labelModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool AddLabelToUser(long userId, LabelModel labelModel)
+        {
+            try
+            {
+                return _labelRL.AddLabelToUser(userId, labelModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Label> GetNoteLables(long noteId, long userId)
+        {
+            try
+            {
+                return _labelRL.GetNoteLables(noteId, userId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Label> GetUserLabels(long userId)
+        {
+            try
+            {
+                return _labelRL.GetUserLabels(userId);
             }
             catch (Exception)
             {
--- RepositoryLayer/Services/LabelRL.cs
+++ RepositoryLayer/Services/LabelRL.cs
@@ -48,5 +48,62 @@ namespace RepositoryLayer.Services
                 throw;
             }
         }
+
+        public bool AddLabelToUser(long userId, LabelModel labelModel)
+        {
+            try
+            {
+                var checkLabel = _userContext.Labels.FirstOrDefault(l => l.LabelName == labelModel.labelName && l.UserId == userId);
+
+                if (checkLabel == null)
+                {
+                    label.LabelName = labelModel.labelName;
+                    label.UserId = userId;
+
+                    _userContext.Labels.Add(label);
+                    int changes = _userContext.SaveChanges();
+
+                    if (changes > 0) return true;
+
+                    else return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Label> GetNoteLables(long noteId, long userId)
+        {
+            try
+            {
+                var result = _userContext.Labels.Where(l => l.NoteId == noteId && l.UserId == userId).ToList();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Label> GetUserLabels(long userId)
+        {
+            try
+            {
+                var result = _userContext.Labels.Where(l => l.UserId == userId).ToList();
+
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
