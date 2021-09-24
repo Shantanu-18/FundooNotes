c7426df02326a730f31d093bfdca53cd7e17d22b
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-24 13:18:04 +0530

    [Shantanu] Added : Implementation of EditLabelName, RemoveLabel, DeleteLabel, AddNoteToExistingLabel interfaces.

--- BusinessLayer/Services/LabelBL.cs
+++ BusinessLayer/Services/LabelBL.cs
@@ -20,7 +20,7 @@ namespace BusinessLayer.Services
         {
             try
             {
-                return _labelRL.AddLabel(noteId,userId,labelModel);
+                return _labelRL.AddLabel(noteId, userId, labelModel);
             }
             catch (Exception)
             {
@@ -57,6 +57,54 @@ namespace BusinessLayer.Services
             try
             {
                 return _labelRL.GetUserLabels(userId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool EditLabelName(long labelId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                return _labelRL.EditLabelName(labelId, userId, labelModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool RemoveLabel(long labelId, long noteId, long userId)
+        {
+            try
+            {
+                return _labelRL.RemoveLabel(labelId, noteId, userId);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteLabel(long userId, string labelName)
+        {
+            try
+            {
+                return _labelRL.DeleteLabel(userId, labelName);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool AddNoteToExistingLabel(long noteId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                return _labelRL.AddNoteToExistingLabel(noteId, userId, labelModel);
             }
             catch (Exception)
             {
--- RepositoryLayer/Services/LabelRL.cs
+++ RepositoryLayer/Services/LabelRL.cs
@@ -105,5 +105,101 @@ namespace RepositoryLayer.Services
                 throw;
             }
         }
+
+        public bool EditLabelName(long labelId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                var result = _userContext.Labels.FirstOrDefault(e => e.LabelId == labelId && e.UserId == userId);
+                var checkLabel = _userContext.Labels.FirstOrDefault(l => l.LabelName == labelModel.labelName && l.UserId == userId);
+
+                if (result != null && checkLabel == null)
+                {
+                    result.LabelName = labelModel.labelName;
+
+                    _userContext.SaveChanges();
+
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool RemoveLabel(long labelId, long noteId, long userId)
+        {
+            try
+            {
+                var result = _userContext.Labels.FirstOrDefault(e => e.LabelId == labelId && e.UserId == userId && e.NoteId == noteId);
+
+                if (result != null)
+                {
+                    _userContext.Labels.Remove(result);
+                    _userContext.SaveChanges();
+
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool DeleteLabel(long userId, string labelName)
+        {
+            try
+            {
+                var result = _userContext.Labels.Where(e => e.UserId == userId && e.LabelName == labelName).ToList();
+
+                if (result != null)
+                {
+                    _userContext.Labels.RemoveRange(result);
+                    _userContext.SaveChanges();
+
+                    return true;
+                }
+                else return false;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public bool AddNoteToExistingLabel(long noteId, long userId, LabelModel labelModel)
+        {
+            try
+            {
+                var checkLabel = _userContext.Labels.FirstOrDefault(l => l.LabelName == labelModel.labelName && l.UserId == userId);
+
+                if (checkLabel != null)
+                {
+                    label.LabelName = labelModel.labelName;
+                    label.NoteId = noteId;
+                    label.UserId = userId;
+
+                    _userContext.Labels.Add(label);
+                    int changes = _userContext.SaveChanges();
+
+                    if (changes > 0) return true;
+
+                    else return false;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
     }
 }
