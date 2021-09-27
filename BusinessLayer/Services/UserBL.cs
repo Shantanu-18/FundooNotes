4767c317b502a4b78658ca4a28d67407b913508e
Shantanu Borkar <shanatanuborkar18@gmail.com>
2021-09-27 09:39:25 +0530

    [Shantanu] Added : Style cop Documentations.

--- BusinessLayer/Services/UserBL.cs
+++ BusinessLayer/Services/UserBL.cs
@@ -1,22 +1,35 @@
-﻿using BusinessLayer.Interface;
-using CommonLayer;
-using RepositoryLayer.Entity;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// <copyright file="UserBL.cs" Company="MediaWare Infotech Pvt. Lmt.>
+//   Copyright © 2021 Company="MediaWare Infotech Pvt. Lmt."
+// </copyright>
+// <creator name="Shantanu"/>
 namespace BusinessLayer.Services
 {
+    using System;
+    using System.Collections.Generic;
+    using BusinessLayer.Interface;
+    using CommonLayer;
+    using RepositoryLayer.Entity;
+
     public class UserBL : IUserBL
     {
+        /// <summary>
+        /// The user repository layer
+        /// </summary>
         private IUserRL _userRL;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserBL"/> class.
+        /// </summary>
+        /// <param name="userRL">The user rl.</param>
         public UserBL(IUserRL userRL)
         {
             this._userRL = userRL;
         }
 
-
+        /// <summary>
+        /// Gets all users.
+        /// </summary>
+        /// <returns></returns>
         public List<UserModel> getAllUsers()
         {
             try
@@ -29,6 +42,11 @@ namespace BusinessLayer.Services
             }
         }
 
+        /// <summary>
+        /// Registers the user.
+        /// </summary>
+        /// <param name="userModel">The user model.</param>
+        /// <returns>boolean</returns>
         public bool RegisterUser(UserModel userModel)
         {
             try
@@ -41,23 +59,16 @@ namespace BusinessLayer.Services
             }
         }
 
+        /// <summary>
+        /// Users the log in.
+        /// </summary>
+        /// <param name="logInModel">The log in model.</param>
+        /// <returns>User</returns>
         public User UserLogIn(LogInModel logInModel)
         {
             try
             {
-                return _userRL.UserLogIn(logInModel);
-            }
-            catch (Exception e)
-            {
-                throw;
-            }
-        }
-
-        public User ForgotPassword(ForgotPassModel forgotPassModel)
-        {
-            try
-            {
-                return _userRL.ForgotPassword(forgotPassModel);
+                return this._userRL.UserLogIn(logInModel);
             }
             catch (Exception)
             {
@@ -65,11 +76,34 @@ namespace BusinessLayer.Services
             }
         }
 
-        public User ResetPassword(ResetPasswordModel resetPasswordModel, long UserId)
+        /// <summary>
+        /// Forgot the password.
+        /// </summary>
+        /// <param name="forgotPassModel">The forgot pass model.</param>
+        /// <returns>User</returns>
+        public User ForgotPassword(ForgotPassModel forgotPassModel)
         {
             try
             {
-                return _userRL.ResetPassword(resetPasswordModel, UserId);
+                return this._userRL.ForgotPassword(forgotPassModel);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Resets the password.
+        /// </summary>
+        /// <param name="resetPasswordModel">The reset password model.</param>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns>User</returns>
+        public User ResetPassword(ResetPasswordModel resetPasswordModel, long userId)
+        {
+            try
+            {
+                return this._userRL.ResetPassword(resetPasswordModel, userId);
             }
             catch (Exception)
             {
--- CommonLayer/CollaborationModel.cs
+++ CommonLayer/CollaborationModel.cs
@@ -1,11 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// ---------------------------------------------------
+// <copyright file="CollaborationModel.cs" Company="MediaWare Infotech Pvt. Lmt.>
+//   Copyright © 2021 Company="MediaWare Infotech Pvt. Lmt."
+// </copyright>
+// <creator name="Shantanu"/>
+// ---------------------------------------------------
 namespace CommonLayer
 {
+    /// <summary>
+    /// Collaboration Model
+    /// </summary>
     public class CollaborationModel
     {
+        /// <summary>
+        /// Gets or sets the Collaboration email.
+        /// </summary>
+        /// <value>
+        /// The Collaboration email.
+        /// </value>
         public string collabEmail { get; set; }
     }
 }
